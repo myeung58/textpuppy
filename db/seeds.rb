b4e49d7ccc00e63c6725bc784d5a452a98313f8d
myeung58 <myeung58@gmail.com>
2014-09-12 12:42:54 -0700

    basic dashboard design and minor fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 User.delete_all
 
-user = User.create(username: "test", password: "test", email: "test@test.com", name: "test name", phone: "2262420765")
-user.contacts << Contact.create(contact_name: "Michael", phone: "2269805481")
+user = User.create(username: "myeung58", password: "123", email: "test@test.com", name: "test name", phone: "2262420765")
+user.contacts << Contact.create(contact_name: "Nick", phone: "9167981133")
+user.contacts << Contact.create(contact_name: "Ryan", phone: "4103020865")
